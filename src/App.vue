cbdf0548a0efe929defa408392c3d5d015c68383
DaviLeoMeda <davileo.meda@gmail.com>
2023-05-03 10:15:05 +0200

    semi Completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
-<script setup>
-import AppTitle from './components/AppTitle.vue'
+<script>
+  import AppTitle from './components/AppTitle.vue'
+
+  export default {
+    name: "App",
+    components: {
+      AppTitle
+    }
+  }
+
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <AppTitle/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,9 +1,18 @@
-<template>
+<style scoped>
+export default {
+    name: "AppTitle"
+}
+</style>
+
+
+<template>  
+
+    <div>
+        <h1>Hello World</h1>
+    </div>
     
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<style>
+
 </style>
